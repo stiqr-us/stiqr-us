8b0f679c1c53f57d21e127ff195b96fada786c88
Riley <code@rilstrats.net>
2023-02-16 11:09:37 -0700

    firebase and angular environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDax1ztdFPcMtrw_iYfAaE9jO229qXn6dI",
+    authDomain: "stiqr-dev.firebaseapp.com",
+    projectId: "stiqr-dev",
+    storageBucket: "stiqr-dev.appspot.com",
+    messagingSenderId: "973370953650",
+    appId: "1:973370953650:web:48900179ad1983c1741eab",
+    measurementId: "G-2GVD3BXR8S"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAHxEbKC0P96QVjxBPC3YGNtjIu_d9b7EU",
+    authDomain: "stiqr-us.firebaseapp.com",
+    projectId: "stiqr-us",
+    storageBucket: "stiqr-us.appspot.com",
+    messagingSenderId: "946729838126",
+    appId: "1:946729838126:web:6cb1460d9186ec11b5a1bf",
+    measurementId: "G-WCVYG4WEQZ"
+  },
+};
