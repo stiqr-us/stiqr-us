db9654936cc24ab8fef4d1cb5494ec893f255cae
Riley <91571914+rilstrats@users.noreply.github.com>
2023-05-18 14:24:02 -0600

    added email/password and Facebook auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Auth, AuthProvider, GoogleAuthProvider, User, signInWithPopup, signOut, user } from '@angular/fire/auth';
+import { Auth, AuthProvider, createUserWithEmailAndPassword, GoogleAuthProvider, User, signInWithPopup, signOut, user, signInWithEmailAndPassword, FacebookAuthProvider } from '@angular/fire/auth';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -16,16 +16,19 @@ export class AppComponent {
   signUp_password: string = "";
 
   providers = [
-    { "name": "Google", "type": new GoogleAuthProvider }
+    { "name": "Google", "type": new GoogleAuthProvider },
+    { "name": "Facebook", "type": new FacebookAuthProvider }
   ]
   private auth: Auth = inject(Auth);
   user$ = user(this.auth);
+  user: User | null = null;
   userSubscription: Subscription;
 
   constructor() {
     this.userSubscription = this.user$.subscribe((aUser: User | null) => {
       //handle user state changes here. Note, that user will be null if there is no currently logged in user.
       console.log("aUser:", aUser);
+      this.user = aUser;
     })
   }
 
@@ -45,12 +48,17 @@ export class AppComponent {
     this.signUp_password = userPass[1];
   }
 
+  async createEmailPassword(email: string, password: string) {
+    console.log("create")
+    return await createUserWithEmailAndPassword(this.auth, email, password)
+  }
+
   async loginEmailPassword(email: string, password: string) {
-    console.log("login");
-    // return await
+    console.log("login password");
+    return await signInWithEmailAndPassword(this.auth, email, password)
   }
   async loginProvider(provider: AuthProvider) {
-    console.log("login");
+    console.log("login provider");
     return await signInWithPopup(this.auth, provider)
   }
 
