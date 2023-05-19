04b39e81388829890c2f56ed088e8511d7640f01
Riley <91571914+rilstrats@users.noreply.github.com>
2023-05-19 15:08:00 -0600

    added sticker picture and scanner name and contact

--- src/app/db/sticker.ts
+++ src/app/db/sticker.ts
@@ -3,10 +3,16 @@ import { GeoPoint, Timestamp } from "@angular/fire/firestore"
 export interface Sticker {
   id?: string,
   printed: boolean,
+  picture: string,
   activationCode: string,
   activated: boolean,
   userId?: string,
   name?: string,
   lost: boolean,
-  locations: { location: GeoPoint, timestamp: Timestamp }[],
+  locations: {
+    location: GeoPoint,
+    timestamp: Timestamp,
+    scannerName?: string,
+    scannerNumber?: string,
+  }[],
 }
